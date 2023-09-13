b64f7794dd77d826cdcb7fa8e5679349b9100528
JoneyKroney <johnlowriefirst@gmail.com>
2023-09-13 12:25:01 -0500

    edited /employees/ to have assmbler

--- Payroll/src/main/java/com/example/Payroll/EmployeeController.java
+++ Payroll/src/main/java/com/example/Payroll/EmployeeController.java
@@ -35,10 +35,8 @@ class EmployeeController {
     @GetMapping("/employees")
     CollectionModel<EntityModel<Employee>> all() {
 
-        List<EntityModel<Employee>> employees = repository.findAll().stream()
-                .map(employee -> EntityModel.of(employee,
-                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+                .map(assembler::toModel) //
                 .collect(Collectors.toList());
 
         return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
