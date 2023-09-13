99a0a6dacbd4dafc43a10a45c8fc9f7f748d8334
JoneyKroney <johnlowriefirst@gmail.com>
2023-09-13 14:08:08 -0500

    added OrderController changed order and loaddatabase

--- Payroll/src/main/java/com/example/Payroll/Order.java
+++ Payroll/src/main/java/com/example/Payroll/Order.java
@@ -6,7 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.transaction.Status;
+
 
 @Entity
 @Table(name = "CUSTOMER_ORDER")
