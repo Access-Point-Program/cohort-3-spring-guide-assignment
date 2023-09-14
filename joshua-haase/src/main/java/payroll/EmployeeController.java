9a0fb1f25710a5a96908c9a19fb3d902d1c98024
JoshHaase0 <joshua.m.haase@gmail.com>
2023-09-14 10:46:17 -0500

    everything up until 'Evolving REST APIs'

--- joshua-haase/src/main/java/payroll/EmployeeController.java
+++ joshua-haase/src/main/java/payroll/EmployeeController.java
@@ -1,7 +1,9 @@
 package payroll;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,16 +22,24 @@ class EmployeeController {
 
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
-        this.repository = repository;
-    }
+    private final EmployeeModelAssembler assembler;
 
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+
+        this.repository = repository;
+        this.assembler = assembler;
+    }
 
     // Aggregate root
     // tag::get-aggregate-root[]
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream() //
+                .map(assembler::toModel) //
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
     // end::get-aggregate-root[]
 
@@ -46,9 +56,7 @@ class EmployeeController {
         Employee employee = repository.findById(id) //
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
 
-        return EntityModel.of(employee, //
-                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
