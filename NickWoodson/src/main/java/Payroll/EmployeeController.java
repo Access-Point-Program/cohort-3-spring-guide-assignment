be710c18eabd81d43dfd6d89d90d4d1fa9c449d9
nickw1225 <108931009+nickw1225@users.noreply.github.com>
2023-09-14 11:09:09 -0500

    Update EmployeeController.java

--- NickWoodson/src/main/java/Payroll/EmployeeController.java
+++ NickWoodson/src/main/java/Payroll/EmployeeController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.http.ResponseEntity;
+import org.springframework.hateoas.IanaLinkRelations;
 
 @RestController
 class EmployeeController {
