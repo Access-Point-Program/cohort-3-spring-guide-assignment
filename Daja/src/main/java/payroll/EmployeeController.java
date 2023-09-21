267c2580fff0c6afc744f4a4f60e9c8407d977f8
SymbiodRelations <djamerson95@gmail.com>
2023-09-21 16:11:50 -0500

    spring boot app

--- Daja/src/main/java/payroll/EmployeeController.java
+++ Daja/src/main/java/payroll/EmployeeController.java
@@ -14,17 +14,27 @@ import org.springframework.web.bind.annotation.RestController;
 class EmployeeController {
 
   private final EmployeeRepository repository;
+  private final EmployeeAssembler assembler;
 
-  EmployeeController(EmployeeRepository repository) {
+  EmployeeController(EmployeeRepository repository, EmployeeModeAssembler, assembler) {
     this.repository = repository;
+    this.assembler = assembler;
+  }
   }
 
 
   // Aggregate root
   // tag::get-aggregate-root[]
   @GetMapping("/employees")
-  List<Employee> all() {
-    return repository.findAll();
+  CollectionModel<EntityModel<Employee>> all() {
+
+    List<EntityModel<Employee>> employees = repository.findAll().stream()
+            .map(employee -> EntityModel.of(employee,
+                    linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                    linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+            .collect(Collectors.toList());
+
+    return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
   }
   // end::get-aggregate-root[]
 
@@ -36,10 +46,11 @@ class EmployeeController {
   // Single item
   
   @GetMapping("/employees/{id}")
-  Employee one(@PathVariable Long id) {
-    
-    return repository.findById(id)
-      .orElseThrow(() -> new EmployeeNotFoundException(id));
+  EntityMode<Employee> one(@PathVariable Long id) {
+
+    Employee employee = repository.findById(id)
+            .orElseThrow(() -> new EmployeeNotFoundException(id));
+    return assembler.toModel(employee);
   }
 
   @PutMapping("/employees/{id}")
