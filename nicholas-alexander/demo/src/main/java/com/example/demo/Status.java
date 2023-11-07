67a1b0e3f7e114fd61626dd21b0c3879be239f7d
NinjaNick3205 <nicholasalexander9@yahoo,com>
2023-11-07 09:28:03 -0600

    this might break eevrything

---
+++ nicholas-alexander/demo/src/main/java/com/example/demo/Status.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+enum Status {
+
+    IN_PROGRESS, //
+    COMPLETED, //
+    CANCELLED
+}
+
+interface OrderRepository extends JpaRepository<Order, Long> {
+}
